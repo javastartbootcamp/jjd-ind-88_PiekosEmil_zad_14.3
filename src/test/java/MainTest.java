82ee8736a47218751583007fa8c75105c28ab1db
Marcin Kunert <the.martines@gmail.com>
2020-11-12 13:49:29 +0100

    Przygotowanie zadania

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,3 +1,4 @@
+import com.sun.tools.javac.Main;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,21 +11,64 @@ import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 @Timeout(5)
-public class ExampleTest {
+public class MainTest {
 
     private final PrintStream originalOut = System.out;
     private final InputStream originalIn = System.in;
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
-    void shouldWorkFrom1To3() {
+    @Test
+    void shouldWorkForOnlyOneCountry() throws Exception {
         // given
-        // provideInput("1", "2", "3", "-12");
+        copyFileFromResources("example1.csv", "countries.csv");
+        provideInput("PL");
 
         // when
-        // Main.main(new String[]{});
+        Main.main(new String[]{});
 
         // then
+        assertThat(outContent.toString()).contains("Polska (PL) ma 38000000 ludności.");
+    }
+
+    @Test
+    void shouldWorkForMultipleCountries() throws Exception {
+        // given
+        copyFileFromResources("example2.csv", "countries.csv");
+        provideInput("DE");
+
+        // when
+        Main.main(new String[]{});
+
+        // then
+        assertThat(outContent.toString()).contains("Polska (PL) ma 38000000 ludności.");
+    }
+
+    @Test
+    void shouldDisplayMessageWhenNoFile() throws Exception {
+        // given
+        new File("countries.csv").delete();
+
+        // when
+        Main.main(new String[]{});
+
+        // then
+        assertThat(outContent.toString()).contains("Brak pliku countries.csv.");
+    }
+
+    @Test
+    void shouldDisplayMessageWhenCountryNotFound() throws Exception {
+        // given
+        copyFileFromResources("example1.csv", "countries.csv");
+        provideInput("XX");
+
+        // when
+        Main.main(new String[]{});
+
+        // then
+        assertThat(outContent.toString()).contains("Kod kraju XX nie został znaleziony.");
     }
 
     @BeforeEach
