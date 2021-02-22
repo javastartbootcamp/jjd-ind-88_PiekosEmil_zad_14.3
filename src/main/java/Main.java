47decb0a49b2054fc0d146700a99e9eba380bd91
Marcin Kunert <the.martines@gmail.com>
2021-02-22 09:34:15 +0100

    Poprawa testów

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 
 public class Main {
 
+    // nie zmieniaj nic w main
     public static void main(String[] args) {
-
-
+        Scanner scanner = new Scanner(System.in);
+        Main main = new Main();
+        main.run(scanner);
     }
+
+    void run(Scanner scanner) {
+        // usupełnij metodę
+    }
+
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,12 +3,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.util.List;
+import java.util.Scanner;
 
 import static org.assertj.core.api.AssertionsForClassTypes.fail;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -17,8 +20,8 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 public class MainTest {
 
     private final PrintStream originalOut = System.out;
-    private final InputStream originalIn = System.in;
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final Main main = new Main();
 
     @Test
     void shouldWorkForOnlyOneCountry() throws Exception {
@@ -30,24 +33,24 @@ public class MainTest {
         Main.main(new String[]{});
 
         // then
-        assertThat(outContent.toString()).contains("PL (Polska) ma 38000000 ludności.");
+        assertThat(outContent.toString()).contains("Polska (PL) ma 38000000 ludności.");
     }
 
     @Test
     void shouldWorkForMultipleCountries() throws Exception {
         // given
         copyFileFromResources("example2.csv", "countries.csv");
-        provideInput("DE");
+        Scanner scanner = provideInput("DE");
 
         // when
-        Main.main(new String[]{});
+        main.run(scanner);
 
         // then
-        assertThat(outContent.toString()).contains("DE (Niemcy) ma 80000000 ludności.");
+        assertThat(outContent.toString()).contains("Niemcy (DE) ma 80000000 ludności.");
     }
 
     @Test
-    void shouldDisplayMessageWhenNoFile() throws Exception {
+    void shouldDisplayMessageWhenNoFile() {
         // given
 
         File file = new File("countries.csv");
@@ -58,8 +61,10 @@ public class MainTest {
             }
         }
 
+        Scanner scanner = provideInput();
+
         // when
-        Main.main(new String[]{});
+        main.run(scanner);
 
         // then
         assertThat(outContent.toString()).contains("Brak pliku countries.csv.");
@@ -69,10 +74,10 @@ public class MainTest {
     void shouldDisplayMessageWhenCountryNotFound() throws Exception {
         // given
         copyFileFromResources("example1.csv", "countries.csv");
-        provideInput("XX");
+        Scanner scanner = provideInput("XX");
 
         // when
-        Main.main(new String[]{});
+        main.run(scanner);
 
         // then
         assertThat(outContent.toString()).contains("Kod kraju XX nie został znaleziony.");
@@ -86,33 +91,18 @@ public class MainTest {
     @AfterEach
     void cleanup() {
         System.setOut(originalOut);
-        System.setIn(originalIn);
     }
 
-    private void provideInput(String... lines) {
+    private Scanner provideInput(String... lines) {
         String input = String.join("\r\n", lines);
 
         ByteArrayInputStream testIn = new ByteArrayInputStream(input.getBytes());
-        System.setIn(testIn);
+        return new Scanner(testIn);
     }
 
     private void copyFileFromResources(String name, String output) throws Exception {
         URL resource = getClass().getResource(name);
         Files.copy(Path.of(resource.toURI()), new File(output).toPath(), StandardCopyOption.REPLACE_EXISTING);
-    }
-
-    private void writeToFile(String fileContent, String filename) throws IOException {
-        File file = new File(filename);
-        FileWriter fileWriter = new FileWriter(file);
-        fileWriter.write(fileContent);
-        fileWriter.close();
-    }
-
-    private void writeToFile(List<String> lines, String filename) throws IOException {
-        File file = new File(filename);
-        FileWriter fileWriter = new FileWriter(file);
-        fileWriter.write(String.join("\n", lines));
-        fileWriter.close();
     }
 
 }
